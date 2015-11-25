f5f7585b72dcc5001913b2c9cafe056f2c5e7a50
marius <m.die0123@googlemail.com>
2015-11-25 23:08:05 +0100

    small changes in tests

--- tests/read_test.py
+++ tests/read_test.py
@@ -1,26 +1,26 @@
 import pymzavro
 
-
-
 def test_read():
-	print("======sequential read test======")
-	avroFile = open("BSA3.avro")
-	reader = pymzavro.reader.PymzAvroReader(avroFile)
+    print("======sequential read test======")
+    avroFile = open("BSA3.avro")
+    reader = pymzavro.reader.PymzAvroReader(avroFile)
 
-	isum = 0
-	ms_level = 0
-	for spectrum in reader:
-		isum = isum +  sum(spectrum.getIntensityArray())
-		ms_level = ms_level + int(spectrum.getByAccession("MS:1000511"))
-	print (isum)
-	print (ms_level)
+    isum = 0
+    ms_level = 0
+    for spectrum in reader:
+        isum = isum +  sum(spectrum.getIntensityArray())
+        ms_level = ms_level + int(spectrum.getByAccession("MS:1000511"))
+    print (isum)
+    print (ms_level)
+
+    print("=====index seek======")
+    try:
+        avroFileSeek = open("BSA3.avro")
+        indexReader = pymzavro.reader.PymzAvroReader(avroFileSeek, indexFile="BSA3_index.json")
+        spectrum = indexReader.rndSeek(694)
+        print(sum(spectrum.getIntensityArray()))
+    except:
+        print("Could not test file seek")
 
 
-	print("=====index seek======")
-	avroFileSeek = open("BSA3.avro")
-	indexReader = pymzavro.reader.PymzAvroReader(avroFileSeek, indexFile="BSA3_index.json")
-
-	spectrum = indexReader.rndSeek(694)
-	print(sum(spectrum.getIntensityArray()))
-
-	return 1
+    return 1
